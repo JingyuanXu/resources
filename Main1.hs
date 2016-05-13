41900dbb2499f9e5429d0d207612645e888e9467
JingyuanXu <skybluries@gmail.com>
2016-05-12 22:56:24 -0700

    runhaskell Main1.hs < s.csv

---
+++ Main1.hs
@@ -0,0 +1,20 @@
+
+import Text.ParserCombinators.Parsec
+	
+main :: IO ()
+
+csvFile = endBy line eol
+line    = sepBy cell (char ',')
+cell    = many (noneOf ",\n\r")
+
+eol = char '\n'
+
+parseCSV :: String -> Either ParseError [[String]]
+parseCSV input = parse csvFile "(unknown)" input
+
+main =
+     do c <- getContents
+        case parse csvFile "(stdin)" c of
+            Left e -> do putStrLn "Error parsing input:"
+                         print e
+            Right r -> mapM_ print r
